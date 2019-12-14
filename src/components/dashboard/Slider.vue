e7070f62b0bb934716371d7e3669ea627e95dbc4
yuuFen <603236742@qq.com>
2019-12-14 12:52:36 +0800

    新建了一些文件

---
+++ src/components/dashboard/Slider.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="slider">
+    <div>323</div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Slider',
+  props: {
+
+  },
+  data() {
+    return {
+
+
+    }
+  }
+}
+</script>
