608ed1c96ce7d488bfd17b6300ec408532e6abf7
yuuFen <603236742@qq.com>
2020-02-04 05:18:12 +0800

    完成AppGroup但是当执行到它的的resize时,并没有设定mobile的width,存在bug

---
+++ src/components/dashboard/AppGroup.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="app-group" :style="`background-image: url(${appGroupBgImg})`">
+    <div class="app-group-title" :style="`background-image: url(${appGroupTitleImg})`">{{ title[lang] }}</div>
+    <div class="app-group-content">
+      <m-app v-for="appId in apps" :key="appId" :app-id="appId"></m-app>
+    </div>
+  </div>
+</template>
+
+<script>
+import { getPixelImage } from '../../assets/js/utils/image'
+import colors from '../../assets/js/constants/colors'
+import { getLang } from '../../assets/js/utils/lang'
+
+export default {
+  name: 'AppGroup',
+  props: {
+    title: { type: Object, default: () => '' },
+    apps: { type: Array, default: () => [] },
+  },
+  data() {
+    return {
+      lang: getLang(),
+      appGroupTitleImg: '',
+      appGroupBgImg: '',
+    }
+  },
+  methods: {
+    resize() {
+      const group = this.$el
+      console.log(group.clientWidth)
+
+      this.appGroupBgImg = getPixelImage({
+        width: group.clientWidth,
+        height: group.clientHeight,
+        radius: 2,
+        fillColor: colors.appGroup,
+      })
+      const title = group.getElementsByClassName('app-group-title')[0]
+      this.appGroupTitleImg = getPixelImage({
+        width: title.clientWidth,
+        height: title.clientHeight,
+        radius: [2, 2, 0, 0],
+        fillColor: colors.appGroupTitle,
+      })
+    },
+  },
+  mounted() {
+    this.resize()
+    window.addEventListener('resize', this.resize)
+  },
+}
+</script>
