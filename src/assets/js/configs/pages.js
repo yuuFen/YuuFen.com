c0b3cc4dfeebfb24f5aaf2030e03494f05ef2c74
yuuFen <603236742@qq.com>
2020-02-04 02:06:27 +0800

    规定数据结构 数据流动: page > group > appId => detail(icon)

---
+++ src/assets/js/configs/pages.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    id: 0,
+    groups: [
+      { title: { zh: 'app作品', en: 'Apps' }, apps: ['App1', 'App2'] },
+      { title: { zh: '网页作品', en: 'Websites' }, apps: ['Websites1', 'Websites2'] },
+    ],
+  },
+  {
+    id: 1,
+    groups: [
+      { title: { zh: '社交网络账号', en: 'Social Networks' }, apps: ['github', 'weibo', 'douban', 'zhihu', 'bilibili'] },
+      { title: { zh: '其他', en: 'Others' }, apps: ['blog', 'email', 'comment'] },
+    ],
+  },
+]
---
+++ src/assets/js/constants/icons.js
@@ -0,0 +1,13 @@
+import themes from './themes'
+
+export default {
+  douban: {
+    name: {
+      zh: '豆瓣',
+      en: 'Douban',
+    },
+    pos: [0, 0],
+    logoTheme: themes.medium,
+    link: 'https://www.douban.com/'
+  },
+}
