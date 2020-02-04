c1800fbedbdeb48589a8240e138a78b0b595108d
yuuFen <603236742@qq.com>
2020-02-05 05:33:32 +0800

    原生写了Slider组件(使用scroll,计时器改变scroll实现动画,计时器"暂停",详见文档

--- src/assets/js/configs/pages.js
+++ src/assets/js/configs/pages.js
@@ -4,13 +4,12 @@ export default [
     groups: [
       { title: { zh: '社交网络账号', en: 'Social Networks' }, apps: ['github', 'dribbble', 'weibo', 'douban', 'zhihu', 'bilibili'] },
       { title: { zh: '其他', en: 'Others' }, apps: ['blog', 'email', 'comment'] },
-      // { title: { zh: '网页作品', en: 'Websites' }, apps: ['Websites1', 'Websites2'] },
     ],
   },
   {
     id: 1,
     groups: [
-      // { title: { zh: 'app作品', en: 'Apps' }, apps: ['App1', 'App2'] },
+      { title: { zh: '社交网络账号', en: 'Social Networks' }, apps: ['github', 'dribbble', 'weibo', 'douban', 'zhihu', 'bilibili'] },
     ],
   },
 ]
--- src/components/dashboard/Slider.vue
+++ src/components/dashboard/Slider.vue
@@ -12,7 +12,7 @@
     <div class="slider-page-container">
       <m-slider-page v-for="page in pages" :key="page.id" :app-groups="page.groups" v-bind="$attrs"></m-slider-page>
     </div>
-    <div class="slider-control">
+    <div class="slider-control" ref="ctrl">
       <span class="slider-control-btn" v-for="page in pages" :key="page.id"></span>
     </div>
   </div>
@@ -25,15 +25,80 @@ export default {
     pages: { type: Array, default: () => [] },
   },
   data() {
-    return {}
+    return {
+      pageId: 0,
+      pageCount: null,
+      pageContainer: null,
+      isMouseDown: false,
+      lastMoveX: null,
+      lastDownX: null,
+    }
+  },
+  mounted() {
+    this.pageContainer = this.$el.getElementsByClassName('slider-page-container')[0]
+    this.pageCount = this.pageContainer.childNodes.length
+    this.scrollToPage(0)
   },
   methods: {
-    mouseDown() {},
-    mouseMove() {},
-    mouseUp() {},
-    touchStart() {},
-    touchMove() {},
-    touchEnd() {},
+    mouseDown(e) {
+      this.isMouseDown = true
+      this.lastMoveX = e.clientX
+      this.lastDownX = e.clientX
+    },
+    touchStart(e) {
+      this.mouseDown(e.touches[0])
+    },
+    mouseMove(e) {
+      if (this.isMouseDown) {
+        this.scrollBy(this.lastMoveX - e.clientX)
+        this.lastMoveX = e.clientX
+      }
+    },
+    touchMove(e) {
+      this.mouseMove(e.changedTouches[0])
+    },
+    mouseUp(e) {
+      if (!this.isMouseDown) {
+        return
+      }
+      if (this.lastDownX - e.clientX > 30) {
+        ++this.pageId
+      } else if (this.lastDownX - e.clientX < -30) {
+        --this.pageId
+      }
+      if (this.pageId < 0) {
+        this.pageId = 0
+      } else if (this.pageId >= this.pageCount) {
+        this.pageId = this.pageCount - 1
+      }
+      this.scrollToPage(this.pageId)
+      this.isMouseDown = false
+      this.lastMoveX = null
+      this.lastDownX = null
+    },
+    touchEnd(e) {
+      this.mouseUp(e.changedTouches[0])
+    },
+    scrollBy(dx) {
+      this.pageContainer.scrollLeft += dx
+    },
+    scrollToPage(pageId) {
+      const pageWidth = document.getElementsByClassName('slider-page')[0].clientWidth
+
+      const timer = setInterval(() => {
+        const target = pageWidth * pageId
+        let step = (target - this.pageContainer.scrollLeft) / 10
+        step = step > 0 ? Math.ceil(step) : Math.floor(step) // 滚动条的数值只能为整数
+        this.scrollBy(step)
+        if (this.isMouseDown || this.pageContainer.scrollLeft === target) {
+          clearInterval(timer) // mouseUp的时候又会set一个计时器，所以这里mouseDown的作用相当于暂停
+        }
+      }, 5)
+      
+
+      const ctrl = this.$refs.ctrl
+
+    },
   },
 }
 </script>
