724a6650bd6917fe20bdab39e5ca7c8381ea2149
yuuFen <603236742@qq.com>
2020-02-09 05:20:39 +0800

    相册的布局 以及图像处理的初始化（还没有理清）

---
+++ src/assets/js/configs/album.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    title: {
+      zh: '项目',
+      en: 'Projects',
+    },
+    photos:[
+      'assets/album/projects/pixel-wedding.png',
+      'assets/album/projects/calendar.png',
+      'assets/album/projects/gooday.png',
+      'assets/album/projects/moonight.png'
+    ]
+  },
+  {
+    title: {
+      zh: '设计作品',
+      en: 'Design'
+    },
+    photos:[
+      'assets/album/projects/pixel-wedding.png',
+      'assets/album/projects/calendar.png',
+      'assets/album/projects/gooday.png',
+      'assets/album/projects/moonight.png'
+    ]
+  },  {
+    title: {
+      zh: '日常照片',
+      en: 'Routine'
+    },
+    photos:[
+      'assets/album/projects/pixel-wedding.png',
+      'assets/album/projects/calendar.png',
+      'assets/album/projects/gooday.png',
+      'assets/album/projects/moonight.png'
+    ]
+  },
+]
--- src/components/app/Album.vue
+++ src/components/app/Album.vue
@@ -1,13 +1,62 @@
 <template>
-  <h1>相册</h1>
+  <div class="inapp-album inapp-body-content padding" :class="{ scroll: !openImgSrc }">
+    <div class="album-group" v-for="(group, index) in album" :key="index">
+      <h3>{{ group.title[lang] }}</h3>
+      <div class="album-group-content">
+        <div class="album-photo-container" v-for="(photo, index) in group.photos" :key="index">
+          <a href="" class="album-photo-img pixel-img" :style="`background-image: ${photo.thumbnail ? `url('${photo.thumbnail}')` : 'transparent'})`"></a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import album from '../../assets/js/configs/album'
+import { getLang } from '../../assets/js/utils/lang'
+import ImageProcessor from '../../assets/js/entities/imageProcessor'
 
+const thumbnailSize = 32
+const imageProcessor = new ImageProcessor()
+
+export default {
+  data() {
+    return {
+      lang: getLang(),
+      album: [],
+      openImgSrc: null,
+    }
+  },
+  mounted() {
+    this.album = album.map((group) => {
+      const clonedGroups = Object.assign({}, group)
+      clonedGroups.photos = clonedGroups.photos.map((url) => {
+        return {
+          src: url,
+          thumbnail: null,
+        }
+      })
+      clonedGroups.photos.forEach((obj) => {
+        const img = new Image()
+        img.onload = () => {
+          let width = img.width
+          let height = img.height
+          // 保持比例缩小
+          if (width > height) {
+            width = (thumbnailSize / height) * width
+            height = thumbnailSize
+          } else {
+            height = (thumbnailSize / width) * height
+            width = thumbnailSize
+          }
+          obj.thumbnailWidth = width
+          obj.thumbnailHeight = height
+          obj.thumbnail = imageProcessor.doSunglass(img, width, height)
+        }
+        img.src = obj.src // 一定要放在onload后面，不然IE会报错
+      })
+      return clonedGroups
+    })
+  },
 }
 </script>
-
-<style>
-
-</style>
