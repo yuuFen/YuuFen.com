3e993dbc86e92556233e68c1f178c51f80032972
yuuFen <603236742@qq.com>
2020-02-10 05:27:11 +0800

    监听返回退出子页面

--- src/App.vue
+++ src/App.vue
@@ -78,10 +78,17 @@ export default {
   },
   mounted() {
     // 阻止手机的实体返回键
+    const that = this
     history.pushState(null, null, location.href)
     window.addEventListener('popstate', function(event) {
       history.pushState(null, null, location.href)
       //此处加入回退时你要执行的代码
+      if (that.inApp) {
+        // 不知道为什么异步顺畅一点
+        setTimeout(() => {
+          that.exitApp()
+        })
+      }
     })
 
     this.resizeMobile()
