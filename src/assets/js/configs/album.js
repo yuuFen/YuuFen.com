85650dd8bb8b7f563e65cd910ee1a0f0dec596a8
yuuFen <603236742@qq.com>
2020-02-10 03:38:57 +0800

    把album从assets移到了public

--- src/assets/js/configs/album.js
+++ src/assets/js/configs/album.js
@@ -1,25 +1,37 @@
-const img = require('../../album/projects/gooday.png')
-
 export default [
+  // {
+  //   title: {
+  //     zh: '业余项目',
+  //     en: 'Projects',
+  //   },
+  //   photos: ['/album/projects/calendar.png', '/album/projects/gooday.png', '/album/projects/moonight.png'],
+  // },
   {
     title: {
-      zh: '项目',
-      en: 'Projects',
+      zh: '摄影作品',
+      en: 'photographys',
     },
-    photos: [img, img, img, img],
+    photos: [
+      '/album/photography/1.jpg',
+      '/album/photography/2.jpg',
+      '/album/photography/3.jpg',
+      '/album/photography/4.jpg',
+      '/album/photography/5.jpg',
+      '/album/photography/6.jpg',
+    ],
   },
-  {
-    title: {
-      zh: '设计作品',
-      en: 'Design',
-    },
-    photos: [img, img, img, img],
-  },
+  // {
+  //   title: {
+  //     zh: '设计作品',
+  //     en: 'Design',
+  //   },
+  //   photos: ['/album/design/1.png', '/album/design/2.png', '/album/design/3.png'],
+  // },
   {
     title: {
       zh: '日常照片',
       en: 'Routine',
     },
-    photos: [img, img, img, img],
+    photos: ['/album/daily/1.jpg', '/album/daily/2.jpg', '/album/daily/3.jpg', '/album/daily/4.jpg'],
   },
 ]
